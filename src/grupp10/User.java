a7749d798ecc5c8e19a88802fb49d617c6be0b35
robertoestlin <robert.oestlin@gmail.com>
2017-01-25 14:44:28 +0100

    130.1 Databas stöd: User globals

---
+++ src/grupp10/User.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package grupp10;
+
+/**
+ *
+ * @author Robert Östlin
+ */
+public class User {
+    public static String id = "";
+    public static String fullName = "";
+    public static boolean isAdmin = false;
+    public static String username = ""; 
+}
