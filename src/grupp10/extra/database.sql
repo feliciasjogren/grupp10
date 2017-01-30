661aab6eb7f97705b78db760fbf26ecf6c589fff
robertoestlin <robert.oestlin@gmail.com>
2017-01-30 14:20:41 +0100

    130.1 Databas stöd: FormellabloggInlagg tabell.

--- src/grupp10/extra/database.sql
+++ src/grupp10/extra/database.sql
@@ -1,9 +1,17 @@
--- author Robert Östlin (scrum master)
-DROP DATABASE IF EXISTS grupp10;
-CREATE DATABASE grupp10;
+-- author Robert Östlin, Lukas Lindgren
+drop database if exists grupp10;
+create database grupp10;
 use grupp10;
 
 set FOREIGN_KEY_CHECKS = 0;
+
+create table Telefonnummer
+(
+    LarareId int,
+    Nummer varchar(40),
+    primary key (LarareId, Nummer),
+    foreign key (LarareId) references Larare (Id)
+);
 
 create table Larare
 (
@@ -15,23 +23,42 @@ create table Larare
     Email varchar(40),
     Titel varchar(40),
     Rumsnummer varchar(10),
-    Telefon int,
     isAdmin boolean,
     isDeleted boolean,
-    primary key(Id)
-    -- foreign key (Telefon2) references Telefon (Id)
+    primary key(Id),
+    unique (Anvandarnamn, Email)
 );
 
-insert into Larare (Id, Anvandarnamn, Losenord, Fornamn, Efternamn, Email, Titel, Rumsnummer, Telefon, isAdmin, isDeleted)
-values (default, "asan", "anders123", "Anders", "Andersson", "anders_andersson@gmail.com", "Professor", "N4056", "0730133769", TRUE, FALSE);
+create table FormellaInlagg
+(
+    Id int not null auto_increment,
+    Rubrik varchar(40) not null,
+    `Text` varchar(3000) not null,
+    Publiceringsdatum date not null,
+    isDeleted boolean,
+    AntalLasningar int not null,
+    LarareId int,
+    primary key (Id),
+    foreign key (LarareId) references Larare(Id)
+);
 
-insert into Larare (Id, Anvandarnamn, Losenord, Fornamn, Efternamn, Email, Titel, Rumsnummer, Telefon, isAdmin, isDeleted)
-values (default, "bagn", "britta123", "Britta", "Gustafsson", "britta_gustafsson@gmail.com", "Doktorand", "N3067", "0704315657", FALSE, FALSE);
+-- sample data
+insert into Larare (Id, Anvandarnamn, Losenord, Fornamn, Efternamn, Email, Titel, Rumsnummer, isAdmin, isDeleted)
+values (default, "asan", "anders123", "Anders", "Andersson", "anders_andersson@gmail.com", "Professor", "N4056", true, false);
 
-insert into Larare (Id, Anvandarnamn, Losenord, Fornamn, Efternamn, Email, Titel, Rumsnummer, Telefon, isAdmin, isDeleted)
-values (default, "jsln", "jens123", "Jens", "Larsson", "jens_larsson@gmail.com", "Lärare", "N3069", "0706850202", FALSE, FALSE);
+insert into Larare (Id, Anvandarnamn, Losenord, Fornamn, Efternamn, Email, Titel, Rumsnummer, isAdmin, isDeleted)
+values (default, "bagn", "britta123", "Britta", "Gustafsson", "britta_gustafsson@gmail.com", "Doktorand", "N3067", false, false);
 
-insert into Larare (Id, Anvandarnamn, Losenord, Fornamn, Efternamn, Email, Titel, Rumsnummer, Telefon, isAdmin, isDeleted)
-values (default, "lsbg", "lars123", "Lars", "Borg", "lars_borg@gmail.com", "Chefsprofessor", "N5069", "0700948282", TRUE, FALSE);
+insert into Larare (Id, Anvandarnamn, Losenord, Fornamn, Efternamn, Email, Titel, Rumsnummer, isAdmin, isDeleted)
+values (default, "jsln", "jens123", "Jens", "Larsson", "jens_larsson@gmail.com", "Lärare", "N3069", false, false);
+
+insert into Larare (Id, Anvandarnamn, Losenord, Fornamn, Efternamn, Email, Titel, Rumsnummer, isAdmin, isDeleted)
+values (default, "lsbg", "lars123", "Lars", "Borg", "lars_borg@gmail.com", "Chefsprofessor", "N5069", true, false);
+
+insert into Telefonnummer (LarareId, Nummer) values ((select Id from Larare where Anvandarnamn = 'asan'), "0730133769");
+insert into Telefonnummer (LarareId, Nummer) values ((select Id from Larare where Anvandarnamn = 'asan'), "0730133770");
+insert into Telefonnummer (LarareId, Nummer) values ((select Id from Larare where Anvandarnamn = 'bagn'), "0704315657");
+insert into Telefonnummer (LarareId, Nummer) values ((select Id from Larare where Anvandarnamn = 'jsln'), "0706850202");
+insert into Telefonnummer (LarareId, Nummer) values ((select Id from Larare where Anvandarnamn = 'lsbg'), "0700948282");
 
 set FOREIGN_KEY_CHECKS = 1;
